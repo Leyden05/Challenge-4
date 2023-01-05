ef1548bfd89dbd45af402e6ff264409ac1cc4d37
leyden <fusionsoccer05@gmail.com>
2023-01-04 20:50:52 -0600

    first commit in a while after forgetting to commit while starting... EEP

---
+++ Develop/script.js
@@ -0,0 +1,23 @@
+// user is presented with 5 questions - array of objects
+// var myQuestions:
+
+  // Format for answers
+    // Object that contains {questions: string, Choices: array, answer: string}
+    // Display question - loop
+    // append question
+    // append choices array
+// user selects an answer(button, radio, checkboxes)data-answer = "fdsfds"
+  // click events is on the parent container (NOT INDIVIDUAL BUTTONS -- SHOULD BE ON CONTAINER THAT HOLDS ALL QUESTIONS)
+  // how to know whihc element was clicked(event.target)
+  // if the answer is correct, then display the next question ACCESS ARRAY OF QUESTIONS OBJECT
+  // if incorrect, add 15 seconds to score and display next question
+
+  // when all questions are answered, display form to submit initials
+  // save form values score and initials to local storage
+
+// change to highscores HTML
+// window.location.href"HTML LINK"
+  // read values from localStorage
+  // append score values to page
+
+// possible form idea: display none then display once all questions are answered;
