dbcba2d077466895add142541ca4e24955008cb0
leyden <fusionsoccer05@gmail.com>
2023-01-06 13:42:44 -0600

    2nd commit adding a good amount of framework for the css and for the logic

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,62 @@
 // user is presented with 5 questions - array of objects
-// var myQuestions:
+var myQuestions = [
+  {
+    question: "Who is the first boss you fight in Dark Souls 3",
+    choices: ["Dark Eater Midir", "Ludex Gundyr", "The Crystal Sage", "Ornstein and Smough"],
+    answer: "Ludex Gundyr"
+  },
+  {
+    question: "Who is the second boss you fight in Dark Souls 3",
+    choices: ["Rotten Greatwood", "Ludex Gundyr", "The Crystal Sage", "Ornstein and Smough"],
+    answer: "Rotten Greatwood"
+  },
+  {
+    question: "Who is the third boss you fight in Dark Souls 3",
+    choices: ["Dark Eater Midir", "Ludex Gundyr", "The Crystal Sage", "Ornstein and Smough"],
+    answer: "The Crystal Sage"
+  }
+]
+
+var startBtn = document.querySelector("#startBtn")
+var startContainer = document.querySelector("#startContainer")
+var questionsContainer = document.querySelector("#questions")
+var questionsIndex = 0
+
+startBtn.addEventListener("click", startGame)
+
+function startGame() {
+  startContainer.classList.add("hidden");
+  // startTimer();
+  generateQuestions();
+}
+
+function generateQuestions() {
+  var questionList = document.createElement("ul")
+  questionsContainer.append(questionList)
+
+  var questionTitle = document.createElement("h3")
+  questionTitle.textContent = myQuestions[questionsIndex].question
+  questionList.append(questionTitle)
+
+  for (var i = 0; i < myQuestions[questionsIndex].choices.length; i++) {
+    var choiceBtn = document.createElement("button")
+    choiceBtn.textContent = myQuestions[questionsIndex].choices[i]
+    choiceBtn.setAttribute('class', 'btn')
+    questionList.append(choiceBtn)
+
+    choiceBtn.addEventListener('click', function (event) {
+      // console.log(event.target.textContent)
+      if (event.target.textContent === myQuestions[questionsIndex].answer) {
+        console.log('correct')
+      } else {
+        console.log('incorrect')
+      }
+      questionsIndex++
+    })
+  }
+
+}
+
 
   // Format for answers
     // Object that contains {questions: string, Choices: array, answer: string}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Challenge-4
 Challenge 4 for coding camp yay!
+
+fdsfds
+afdsa
+fds
+afds
+fdas
+
+
+References:
+
+James Q Quick: Used a good amount throughout this quiz to help garner an understanding of the quiz. Some of his code is here.
